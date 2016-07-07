10522f35bd4e4d3d26eb8873831fbab86bd4338b
LiberalOtaku <ledgam3r1279@gmail.com>
2016-07-07 14:48:02 -0400

    Add sign up form.

--- app/signUp/signUp.directive.js
+++ app/signUp/signUp.directive.js
@@ -2,9 +2,22 @@
   angular
     .module('meganote.signUp')
     .directive('mySignUp', () => {
+      class SignUpController {
+        constructor() {
+          this.user = {};
+        }
+
+        submit() {
+          console.log(this.user);
+        }
+      }
+
       return {
         restrict: 'EA',
-        template: '<h1>Sign Up Directive</h1>',
+        templateUrl: 'signUp/signUp.html',
+        controller: SignUpController,
+        controllerAs: 'vm',
+        bindToController: true,
         scope: {},
       };
     });
