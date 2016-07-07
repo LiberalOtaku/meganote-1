0bfc92585a744f853048384afc7e1473705c6d40
LiberalOtaku <ledgam3r1279@gmail.com>
2016-07-07 15:21:29 -0400

    Add users service communication w/ server.

--- app/users/users.service.js
+++ app/users/users.service.js
@@ -1,12 +1,23 @@
 {
-  class UsersService {
-    create(user) {
-      console.log('User created!');
-      console.log(user);
-    }
-  }
-  
   angular
     .module('meganote.users')
-    .service('UsersService', UsersService);
+    .service('UsersService', [
+      '$http',
+      'DATABASE_URL',
+      ($http, DATABASE_URL) => {
+        class UsersService {
+          create(user) {
+            return $http
+              .post(`${DATABASE_URL}users`, {
+                user,
+              })
+              .then(
+                res => console.log(res.data)
+              );
+          }
+        }
+
+        return new UsersService();
+      }
+    ]);
 }
