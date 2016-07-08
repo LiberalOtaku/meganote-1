ae869630a84eac41ff10d1b10affc6b5b591df20
LiberalOtaku <ledgam3r1279@gmail.com>
2016-07-08 11:38:27 -0400

    Added services for current user and auth token.

--- app/users/users.service.js
+++ app/users/users.service.js
@@ -4,16 +4,23 @@
     .service('UsersService', [
       '$http',
       'DATABASE_URL',
-      ($http, DATABASE_URL) => {
+      'AuthToken',
+      'CurrentUser',
+      ($http, DATABASE_URL, AuthToken, CurrentUser) => {
         const usersURL = `${DATABASE_URL}users/`;
+
         class UsersService {
+          // Sign Up
           create(user) {
             return $http
               .post(usersURL, {
                 user,
               })
               .then(
-                res => console.log(res.data)
+                res => {
+                  AuthToken.set(res.data.authToken);
+                  CurrentUser.set(res.data.user);
+                }
               );
           }
         }
